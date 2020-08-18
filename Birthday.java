199dac5f210b4eab6d6d55d4119b2ab93739990e
Jack Skiles <jackskiles420@gmail.com>
2020-08-18 14:53:15 -0400

    FInished program that prints out comparison in milliseconds since I was born

--- Birthday.java
+++ Birthday.java
@@ -1,10 +1,17 @@
 import java.util.Calendar;
 import java.util.Calendar;
+import java.util.Date;
 
 public class Birthday {
     public static void main(String[] args){
         Calendar calendar = Calendar.getInstance();
-        System.out.println(calendar.getTime());
+        Calendar c2 = Calendar.getInstance();
+        c2.set(Calendar.DAY_OF_MONTH, 9);
+        c2.set(Calendar.YEAR, 1997);
+        c2.set(Calendar.MONTH, 0);
+        Long milliseconds = calendar.getTimeInMillis() - c2.getTimeInMillis();
+        System.out.println(c2.compareTo(calendar));
+        System.out.println(milliseconds);
     }
 
 }
