30ee0e099c90c3388ec86f0a5fee2ee42c70c941
Jack Skiles <jackskiles420@gmail.com>
2020-08-18 14:23:40 -0400

    created a program that will retrieve my birthday

---
+++ Birthday.java
@@ -0,0 +1,10 @@
+import java.util.Calendar;
+import java.util.Calendar;
+
+public class Birthday {
+    public static void main(String[] args){
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(calendar.getTime());
+    }
+
+}
