15e9e2fa74acab0356d7d129a373042edfc28dcd
Jack Skiles <jackskiles420@gmail.com>
2020-08-19 15:22:35 -0400

    created simple program to practice replace method

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args){
+        String greeting = "Hello";
+        greeting = greeting.replace("H", "z");
+        System.out.println(greeting);
+    }
+}
