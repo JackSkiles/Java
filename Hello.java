bbccb58d20f9133ac2b2c1b1f3fc7cb5fed71719
Jack Skiles <jackskiles420@gmail.com>
2020-08-20 12:27:29 -0400

    updated some files and created simple bank account program

--- Hello.java
+++ Hello.java
@@ -3,5 +3,7 @@ public class Hello {
         String greeting = "Hello";
         greeting = greeting.replace("H", "z");
         System.out.println(greeting);
+        String hello = new String("Hello, Dave");
+        System.out.println(hello);
     }
 }
