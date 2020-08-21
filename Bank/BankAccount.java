6ae33d9697ef7b9689675cd90d86edfdf4a3aa46
Jack Skiles <jackskiles420@gmail.com>
2020-08-21 15:28:00 -0400

    uptaded some syntax

--- Bank/BankAccount.java
+++ Bank/BankAccount.java
@@ -13,15 +13,19 @@ public class BankAccount{
     */
     public BankAccount(double initialBalance){
         balance = initialBalance;
+
     }
+   
     /** Deposits money into the bank account. @param amount the amount to deposit */
     public void deposit(double amount){
         balance = balance + amount;
+        
     }
     /** Withdraws money from the bank account. @param amount the amount to withdraw */
     public void withdraw(double amount){
         balance = balance - amount;
     }
+    
     /** Gets the current balance of the bank account. @return the current balance */
     public double getBalance(){
         return balance;
